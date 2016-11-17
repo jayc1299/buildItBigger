649b38d00c7e5d77fa5a82c7225f4aa0041a286f
Jay Cunnington <jay.cunnington@gmail.com>
2016-11-17 16:33:17 +0000

    Adding in java lib

---
+++ javajoke/src/main/java/com/udacity/gradle/joker/Jokes.java
@@ -0,0 +1,7 @@
+package com.udacity.gradle.joker;
+
+public class Jokes {
+    public String getJoke() {
+        return "Knock Knock. Who's there? Scott. Scott who? Scott-Nothing-To-Do-With-You";
+    }
+}
