b8b0e0f3240b90ae789ed7f3cc82bd045bc35982
Jay Cunnington <jay.cunnington@gmail.com>
2016-12-01 10:19:35 +0000

    Adding in interstitial add.

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -4,18 +4,23 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.InterstitialAd;
 import com.udacity.gradle.androidlib.JokeActivity;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment implements EndpointsAsyncTask.IEndPointListener {
+
+    InterstitialAd mInterstitialAd;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -42,13 +47,42 @@ public class MainActivityFragment extends Fragment implements EndpointsAsyncTask
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
+        mInterstitialAd = new InterstitialAd(getActivity());
+        mInterstitialAd.setAdUnitId("ca-app-pub-3940256099942544/1033173712");
+
+        mInterstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdClosed() {
+                requestNewInterstitial();
+                getJoke();
+            }
+        });
+        requestNewInterstitial();
+
         getView().findViewById(R.id.fragment_main_gc_call).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                ProgressSpinner.show(getActivity(), "Fetching Joke...");
-                new EndpointsAsyncTask().execute(MainActivityFragment.this);
+                Log.d("add", "isLoaded: " + mInterstitialAd.isLoaded());
+                if(BuildConfig.ENABLE_ADDS && mInterstitialAd.isLoaded()) {
+                    mInterstitialAd.show();
+                }else{
+                    getJoke();
+                }
             }
         });
+    }
+
+    private void getJoke(){
+        ProgressSpinner.show(getActivity(), getString(R.string.pending));
+        new EndpointsAsyncTask().execute(MainActivityFragment.this);
+    }
+
+    private void requestNewInterstitial() {
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .build();
+
+        mInterstitialAd.loadAd(adRequest);
     }
 
     @Override
